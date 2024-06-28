efe3ef819fa4be7212880e7b1827ee3eba86c2bb
Robert-LC <robert_lc1@protonmail.com>
2024-06-27 21:39:52 -0400

    create initial JSX

--- src/component/index.tsx
+++ src/component/index.tsx
@@ -1,9 +1,37 @@
-import React from "react"
+import React, { useState } from 'react'
 
-interface Props {}
+interface Label<T> {
+  title: String
+  value: T
+}
 
-const ThreeWaySwitch: React.FC<Props> = (props) => {
-  return <></>
+interface Props<T> {
+  labels: [Label<T>, Label<T>, Label<T>]
+  defaultPosition?: 'left' | 'center' | 'right'
+  disabled: boolean
+}
+
+const ThreeWaySwitch: <T>(props: Props<T>) => JSX.Element = (props) => {
+  const [togglePosition, setTogglePosition] = useState(props.defaultPosition || 'left')
+
+  return (
+    <div className='switch-container'>
+      <input defaultChecked id='left-option' type='radio' />
+      <label htmlFor='left-option'>
+        <h4>Left</h4>
+      </label>
+
+      <input id='center-option' type='radio' />
+      <label htmlFor='center-option'>
+        <h4>Center</h4>
+      </label>
+
+      <input id='right-option' type='radio' />
+      <label htmlFor='right-option'>
+        <h4>Right</h4>
+      </label>
+    </div>
+  )
 }
 
 export default ThreeWaySwitch
