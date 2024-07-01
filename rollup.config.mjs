5947e60a85cb105c63a340094b66317c34ed289c
Robert-LC <robert_lc1@protonmail.com>
2024-07-01 08:34:44 -0400

    configure for local development

--- rollup.config.mjs
+++ rollup.config.mjs
@@ -2,6 +2,7 @@ import resolve from '@rollup/plugin-node-resolve'
 import commonjs from '@rollup/plugin-commonjs'
 import typescript from '@rollup/plugin-typescript'
 import dts from 'rollup-plugin-dts'
+import babel from '@rollup/plugin-babel'
 import packageJson from './package.json' with { type: 'json' }
 
 export default [
@@ -17,13 +18,33 @@ export default [
         file: packageJson.module,
         format: 'esm',
         sourcemap: true
+      },
+      {
+        // for local browser development
+        name: 'ThreeWaySwitch',
+        file: 'dist/bundle.js',
+        format: 'iife',
+        sourcemap: true,
+        globals: { react: 'React', 'react-dom': 'ReactDOM' }
       }
     ],
-    plugins: [resolve(), commonjs(), typescript({ tsconfig: './tsconfig.json' }), babel({})]
+    plugins: [
+      resolve(),
+      commonjs(),
+      typescript({ tsconfig: './tsconfig.json' }),
+      babel({
+        exclude: 'node_modules/**',
+        presets: ['@babel/preset-env', '@babel/preset-react'],
+        babelHelpers: 'bundled'
+      })
+    ]
   },
   {
     input: 'dist/esm/types/index.d.ts',
-    output: [{ file: 'dist/index.d.ts', format: 'esm' }],
+    output: [
+      { file: 'dist/index.d.ts', format: 'esm' },
+      { file: 'dist/index.cjs.d.ts', format: 'cjs' }
+    ],
     plugins: [dts()]
   }
 ]
